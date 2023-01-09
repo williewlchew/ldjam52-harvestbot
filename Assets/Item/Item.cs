a6624065972f86e1cda8b0e86c7d95380317bb3f
Willie Chew <37847148+williewlchew@users.noreply.github.com>
2023-01-08 23:53:49 -0600

    item changes level

--- Assets/Item/Item.cs
+++ Assets/Item/Item.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Item : MonoBehaviour
 {
     public string name;
+    public bool finalItem = false;
+    public string nextLevelName;
+
 
     public void Remove(){
+
+        if(finalItem)
+        {
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(nextLevelName);
+            
+        }
+
         Destroy(this.gameObject);
     }
 }
